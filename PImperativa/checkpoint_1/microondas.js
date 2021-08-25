75de72da9229974c65d13e7fc7563babeedb0da8
Everton Silverio <ever.silverio@gmail.com>
2021-08-25 16:55:09 -0300

    Melhorias microondas.js

--- PImperativa/checkpoint_1/microondas.js
+++ PImperativa/checkpoint_1/microondas.js
@@ -1,17 +1,5 @@
 /**
  * Microndas 
-- Precisamos desenvolver um menu para um microondas super veloz, onde teremos 5 opções de comida com seus respectivos tempos pré-definidos. 
-      1 - Pipoca – 10 segundos (padrão);
-      2 - Macarrão – 8 segundos (padrão);
-      3 - Carne – 15 segundos (padrão);
-      4 - Feijão – 12 segundos (padrão);
-      5 - Brigadeiro – 8 segundos (padrão); 
-ok O usuário poderá alterar o tempo padrão, aumentando ou diminuindo de acordo com sua vontade. 
-ok Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
-ok Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"; 
-ok Opções não listadas no menu, devem exibir uma mensagem de erro: "Prato inexistente";
-ok Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
-ok No final de cada tarefa, o microondas deverá exibir a mensagem: "Prato pronto, bom apetite!!!"
 */
 
 function usarMicroondas(comida, tempo) {
@@ -28,7 +16,7 @@ function usarMicroondas(comida, tempo) {
     console.log('Selecionado opção: Preparar ' + comida + ' em ' + tempo + ' segundos...');
 
     // verifica na lista se existe o prato
-    // se existir armazena o tempo padrao em variavel e atualizar variavel de pratoListado = true
+    // se existir armazena o tempo padrao em variavel e atualiza variavel de pratoListado = true
     comidas.forEach(item => {
         if (item.nome === comida) {
             pratoListado = true;
