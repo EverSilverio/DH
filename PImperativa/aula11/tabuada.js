691b322d74aa659700c5c751deb5b634e922ddaf
EverSilverio <ever.silverio@gmail.com>
2021-08-31 20:37:34 -0300

    [ProgImperativa] Impares e Tabuada - loops

--- PImperativa/aula11/tabuada.js
+++ PImperativa/aula11/tabuada.js
@@ -3,9 +3,9 @@
  * Dica: Para essa tarefa você irá precisar utilizar dois loops (for).
  */
 
-for (var i = 0; i <= 10 ; i++) {
+for (let i = 0; i <= 10 ; i++) {
     console.log('---  TABUADA ' + i + '  ---');
-    for (var x = 0; x <= 10 ; x++) {
+    for (let x = 0; x <= 10 ; x++) {
         let result = i * x;
         console.log(i + ' x ' + x + ' = ' + result);
     }
