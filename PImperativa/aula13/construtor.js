5111e274908d873adffd4170439ea9f50f79c980
EverSilverio <ever.silverio@gmail.com>
2021-09-10 12:35:24 -0300

    fix(correntistas): correcao virgulass

--- PImperativa/aula13/construtor.js
+++ PImperativa/aula13/construtor.js
@@ -12,8 +12,8 @@ let correntistas = [
     new DadosBancarios(1004, "CP", 1000000, "José de Sá"),
     new DadosBancarios(1005, "CC", 1590, "Joana Ribeiro"),
     new DadosBancarios(1006, "CC",15000, "Sergio da Silva"),
-    new DadosBancarios(1007, "CP", 155000, "Alice da Souza")
-    new DadosBancarios(1008, "CC",18000, "João da Silva"),
+    new DadosBancarios(1007, "CP", 155000, "Alice da Souza"),
+    new DadosBancarios(1008, "CC",18000, "João da Silva")
 ]
 
 console.log(correntistas);
