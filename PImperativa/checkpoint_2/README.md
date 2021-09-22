5b6bec667429c369626042f4f416f15578dc1b36
EverSilverio <ever.silverio@gmail.com>
2021-09-21 21:05:50 -0300

    [ProgImp] Checkpoint II

--- PImperativa/checkpoint_2/README.md
+++ PImperativa/checkpoint_2/README.md
@@ -5,15 +5,20 @@
 Agora que temos todo o conhecimento necessário, vamos realizar uma série de exercícios para treinar um pouco o nosso cérebro.
 
 1. Loop de Pares
-Você deve criar uma função chamada loopDePares que receba um número como parâmetro e faça loops de 0 a 100 mostrando cada número do loop no console.
-Caso o número da iteração somado com o número passado pelo parâmetro seja par, aparecerá no console: "O número x é par"
+Você deve criar uma função chamada loopDePares que receba um número como parâmetro e faça 
+loops de 0 a 100 mostrando cada número do loop no console.
+Caso o número da iteração somado com o número passado pelo parâmetro seja par, aparecerá 
+no console: "O número x é par"
 
 2. Loop ímpares com palavra
-Você deve criar uma função chamada loopDeImpares que receba um número e uma palavra como parâmetro e faça loops de 0 a 100 mostrando cada número do loop no console.
-Agora, modifique o código para que, caso esse número somado com o número passado por parâmetro seja ímpar, mostre a palavra passada por parâmetro no console.
+Você deve criar uma função chamada loopDeImpares que receba um número e uma palavra como 
+parâmetro e faça loops de 0 a 100 mostrando cada número do loop no console.
+Agora, modifique o código para que, caso esse número somado com o número passado por 
+parâmetro seja ímpar, mostre a palavra passada por parâmetro no console.
 
 3. Soma
-	Você deve criar uma função chamada soma que receba um número como parâmetro e retorne a soma de todos os seus números anteriores, incluindo o número recebido por parâmetro.
+	Você deve criar uma função chamada soma que receba um número como parâmetro e retorne a 
+	soma de todos os seus números anteriores, incluindo o número recebido por parâmetro.
 ```
 Exemplo: 
 soma(3) deve retornar 6 pois faz (1 +2 +3)
@@ -21,7 +26,9 @@ soma(8) deve retornar 36
 ```
 
 4. Novo Array
-Você deve criar uma função chamada newArray que receba um número como parâmetro e retorne um novo array com a quantidade de elementos equivalente ao valor do número que você passou por parâmetro.
+Você deve criar uma função chamada newArray que receba um número como parâmetro e retorne 
+um novo array com a quantidade de elementos equivalente ao valor do número que você passou 
+por parâmetro.
 ```
 Exemplo: 
 newArray(5) deve retornar [1,2,3,4,5]
@@ -29,7 +36,8 @@ newArray(10) deve retornar [1,2,3,4,5,6,7,8,9,10]
 ```
 
 5. String.split()
-Você deve criar uma função chamada split que receba uma string e simule o comportamento da função split original. Se você não sabe como funciona, o Google pode ajudá-lo.
+Você deve criar uma função chamada split que receba uma string e simule o comportamento da 
+função split original. Se você não sabe como funciona, o Google pode ajudá-lo.
 Importante: Você não pode usar o String.split()
 ```
 Exemplo: 
@@ -38,7 +46,8 @@ split(“tchau”) deve retornar [“t”,“c”,”h”,”a”,”u”]
 ```
 
 6. Mover os zeros para o final
-Você deve criar uma função chamada moverZeros que receba um array como parâmetro e retorne outro com os números 0 ordenados ao final.
+Você deve criar uma função chamada moverZeros que receba um array como parâmetro e retorne 
+outro com os números 0 ordenados ao final.
 
 ```
 Exemplo: 
@@ -47,7 +56,8 @@ moverZeros([1,2,0,1,0,1,0,3,0,1]) deve retornar [1,2,1,1,3,1,0,0,0,0]
 ```
 
 7. Lidando com dois arrays
-Você deve criar uma função chamada arrayHandler que receba dois arrays de igual tamanho como parâmetros e exiba no console: "Eu sou {elemento do array 1} e eu sou {elemento do array 2}".
+Você deve criar uma função chamada arrayHandler que receba dois arrays de igual tamanho 
+como parâmetros e exiba no console: "Eu sou {elemento do array 1} e eu sou {elemento do array 2}".
 ```
 Exemplo:
 arrayHandler([1,2,3], [“o”,”l”,”á”]) deve mostrar: 
@@ -57,7 +67,9 @@ Eu sou 3 e eu sou á
 ```
 
 8. Arrays de objetos
-Você deve criar uma função chamada arrayObjects que receba um número como parâmetro e retorne um array de objetos que tem uma propriedade chamada valor que contém o valor do número e seus anteriores.
+Você deve criar uma função chamada arrayObjects que receba um número como parâmetro e retorne 
+um array de objetos que tem uma propriedade chamada valor que contém o valor do número e seus 
+anteriores.
 
 ```
 Exemplo:
@@ -66,7 +78,10 @@ arrayObjects(3) deve retornar [{valor: 1}, {valor: 2}, {valor: 3}]
 ```
 
 9. Arrays de objetos 2
-Você deve criar uma função chamada arrayObjectsTwo que recebe um número e uma palavra como parâmetro e retorna um array de objetos que possui: uma propriedade nomeada pela palavra passada por parâmetro e o valor dessa propriedade deve ser o número passado por parâmetro e seus anteriores.
+Você deve criar uma função chamada arrayObjectsTwo que recebe um número e uma palavra como 
+parâmetro e retorna um array de objetos que possui: uma propriedade nomeada pela palavra 
+passada por parâmetro e o valor dessa propriedade deve ser o número passado por parâmetro 
+e seus anteriores.
 
 ```
 Exemplo:
--- PImperativa/checkpoint_2/script.js
+++ PImperativa/checkpoint_2/script.js
@@ -1,7 +1,9 @@
 /*
 1. Loop de Pares
-Você deve criar uma função chamada loopDePares que receba um número como parâmetro e faça loops de 0 a 100 mostrando cada número do loop no console.
-Caso o número da iteração somado com o número passado pelo parâmetro seja par, aparecerá no console: "O número x é par"
+Você deve criar uma função chamada loopDePares que receba um número como parâmetro e 
+faça loops de 0 a 100 mostrando cada número do loop no console.
+Caso o número da iteração somado com o número passado pelo parâmetro seja par, 
+aparecerá no console: "O número x é par"
 */
 
 function loopDePares(num) {
@@ -18,8 +20,10 @@ loopDePares(5);
 
 /*
 2. Loop ímpares com palavra
-Você deve criar uma função chamada loopDeImpares que receba um número e uma palavra como parâmetro e faça loops de 0 a 100 mostrando cada número do loop no console.
-Agora, modifique o código para que, caso esse número somado com o número passado por parâmetro seja ímpar, mostre a palavra passada por parâmetro no console.
+Você deve criar uma função chamada loopDeImpares que receba um número e uma palavra como 
+parâmetro e faça loops de 0 a 100 mostrando cada número do loop no console.
+Agora, modifique o código para que, caso esse número somado com o número passado por 
+parâmetro seja ímpar, mostre a palavra passada por parâmetro no console.
 */
 
 function loopDeImpares(num, palavra) {
@@ -36,7 +40,8 @@ loopDeImpares(5, 'bacon');
 
 /*
 3. Soma
-	Você deve criar uma função chamada soma que receba um número como parâmetro e retorne a soma de todos os seus números anteriores, incluindo o número recebido por parâmetro.
+	Você deve criar uma função chamada soma que receba um número como parâmetro e 
+    retorne a soma de todos os seus números anteriores, incluindo o número recebido por parâmetro.
     Exemplo: 
     soma(3) deve retornar 6 pois faz (1 +2 +3)
     soma(8) deve retornar 36
@@ -56,7 +61,9 @@ soma(8)
 
 /*
 4. Novo Array
-Você deve criar uma função chamada newArray que receba um número como parâmetro e retorne um novo array com a quantidade de elementos equivalente ao valor do número que você passou por parâmetro.
+Você deve criar uma função chamada newArray que receba um número como parâmetro e 
+retorne um novo array com a quantidade de elementos equivalente ao valor do número 
+que você passou por parâmetro.
     Exemplo: 
     newArray(5) deve retornar [1,2,3,4,5]
     newArray(10) deve retornar [1,2,3,4,5,6,7,8,9,10]
@@ -75,7 +82,8 @@ newArray(10);
 
 /*
 5. String.split()
-Você deve criar uma função chamada split que receba uma string e simule o comportamento da função split original. Se você não sabe como funciona, o Google pode ajudá-lo.
+Você deve criar uma função chamada split que receba uma string e simule o comportamento 
+da função split original. Se você não sabe como funciona, o Google pode ajudá-lo.
 Importante: Você não pode usar o String.split()
     Exemplo: 
     split(“olá”) deve retornar [”o”,”l”,”á”]
@@ -95,7 +103,8 @@ split('tchau')
 
 /*
 6. Mover os zeros para o final
-Você deve criar uma função chamada moverZeros que receba um array como parâmetro e retorne outro com os números 0 ordenados ao final.
+Você deve criar uma função chamada moverZeros que receba um array como parâmetro e retorne 
+outro com os números 0 ordenados ao final.
     Exemplo: 
     moverZeros([false,1,0,1,2,0,1,3,"a"]) deve retornar [false,1,1,2,1,3,"a",0,0]
     moverZeros([1,2,0,1,0,1,0,3,0,1]) deve retornar [1,2,1,1,3,1,0,0,0,0]
@@ -116,7 +125,8 @@ moverZero(arr);
 
 /*
 7. Lidando com dois arrays
-Você deve criar uma função chamada arrayHandler que receba dois arrays de igual tamanho como parâmetros e exiba no console: "Eu sou {elemento do array 1} e eu sou {elemento do array 2}".
+Você deve criar uma função chamada arrayHandler que receba dois arrays de igual tamanho 
+como parâmetros e exiba no console: "Eu sou {elemento do array 1} e eu sou {elemento do array 2}".
     Exemplo:
     arrayHandler([1,2,3], [“o”,”l”,”á”]) deve mostrar: 
             Eu sou 1 e eu sou o
@@ -138,7 +148,9 @@ arrayHandler(arr1, arr2);
 
 /*
 8. Arrays de objetos
-Você deve criar uma função chamada arrayObjects que receba um número como parâmetro e retorne um array de objetos que tem uma propriedade chamada valor que contém o valor do número e seus anteriores.
+Você deve criar uma função chamada arrayObjects que receba um número como parâmetro e 
+retorne um array de objetos que tem uma propriedade chamada valor que contém o valor 
+do número e seus anteriores.
     Exemplo:
     arrayObjects(5) deve retornar [{valor: 1}, {valor: 2}, {valor: 3}, {valor: 4}, {valor: 5}]
     arrayObjects(3) deve retornar [{valor: 1}, {valor: 2}, {valor: 3}]
@@ -157,7 +169,10 @@ arrayObjects(10);
 
 /*
 9. Arrays de objetos 2
-Você deve criar uma função chamada arrayObjectsTwo que recebe um número e uma palavra como parâmetro e retorna um array de objetos que possui: uma propriedade nomeada pela palavra passada por parâmetro e o valor dessa propriedade deve ser o número passado por parâmetro e seus anteriores.
+Você deve criar uma função chamada arrayObjectsTwo que recebe um número e uma palavra como
+parâmetro e retorna um array de objetos que possui: uma propriedade nomeada pela palavra 
+passada por parâmetro e o valor dessa propriedade deve ser o número passado por parâmetro 
+e seus anteriores.
     Exemplo:
     arrayObjectsTwo(5, “olá”) deve retornar [{olá: 1}, {olá: 2}, {olá: 3}, {olá: 4}, {olá: 5 }]
     arrayObjectsTwo(3, “tchau”) deve retornar [{tchau: 1}, {tchau: 2}, {tchau: 3}]
