0a0a378e073f21faf8f5e0d116c43fcb6159305b
EverSilverio <ever.silverio@gmail.com>
2021-09-27 22:36:42 -0300

    [ProgImperativa] Aula20 - Meu Primeiro app - usando module e obj literal

--- PImperativa/aula20/app.js
+++ PImperativa/aula20/app.js
@@ -5,48 +5,7 @@ Tem-se um conjunto de dados contendo a altura e o sexo (M ou F) de 15 pessoas. F
 3. o número de homens.
 */
 
-const lista = require('./pessoas');
+const pessoas = require('./pessoas');
 
-let pessoas = {
-    lista: lista,
-    calculos: function (calc) {
-        // calc: media, extremos ou homens
-        // orde na lista
-        lista.sort(function (a, b) {
-            return a.altura - b.altura;
-        })
-
-        //soma de altura das mulheres ?
-        // total de mulheres ?
-        // media ?
-        let somaAltMulheres = 0;
-        let qtdMulheres = 0;
-        let qtdHomens = 0;
-
-        lista.forEach(function (el) {
-            if (el.sexo === 'F') {
-                qtdMulheres += 1;
-                somaAltMulheres += el.altura;
-            } else {
-                qtdHomens += 1;
-            }
-        })
-
-        let mediaAltMulheres = (somaAltMulheres / qtdMulheres).toFixed(2);
-
-        switch (calc) {
-            case 'extremos':
-                console.log(`A menor altura do grupo é ${lista[0].altura}`);
-                console.log(`A maior altura do grupo é ${lista[14].altura}`);                
-                break;
-            case 'media':
-            console.log(`A média de altura das mulheres é ${mediaAltMulheres}`);
-                break;
-                case 'homens':
-                console.log(`A quantidade de homens é ${qtdHomens}`);
-                break;
-        }
-    }
-}
-
+// calculos recebe: media, extremos ou homens
 pessoas.calculos('extremos');  //invoca função de calculo
--- PImperativa/aula20/pessoas.js
+++ PImperativa/aula20/pessoas.js
@@ -1,4 +1,4 @@
-module.exports = [
+const lista = [
     { altura: 1.80, sexo: 'M' },
     { altura: 1.67, sexo: 'F' },
     { altura: 1.71, sexo: 'M' },
@@ -15,3 +15,47 @@ module.exports = [
     { altura: 1.55, sexo: 'F' },
     { altura: 1.72, sexo: 'F' },
 ];
+
+let pessoas = {
+    lista: lista,
+    calculos: function (calc) {
+        // calc: media, extremos ou homens
+        // orde na lista
+        lista.sort(function (a, b) {
+            return a.altura - b.altura;
+        })
+
+        //soma de altura das mulheres ?
+        // total de mulheres ?
+        // media ?
+        let somaAltMulheres = 0;
+        let qtdMulheres = 0;
+        let qtdHomens = 0;
+
+        lista.forEach(function (el) {
+            if (el.sexo === 'F') {
+                qtdMulheres += 1;
+                somaAltMulheres += el.altura;
+            } else {
+                qtdHomens += 1;
+            }
+        })
+
+        let mediaAltMulheres = (somaAltMulheres / qtdMulheres).toFixed(2);
+
+        switch (calc) {
+            case 'extremos':
+                console.log(`A menor altura do grupo é ${lista[0].altura}`);
+                console.log(`A maior altura do grupo é ${lista[14].altura}`);                
+                break;
+            case 'media':
+            console.log(`A média de altura das mulheres é ${mediaAltMulheres}`);
+                break;
+                case 'homens':
+                console.log(`A quantidade de homens é ${qtdHomens}`);
+                break;
+        }
+    }
+}
+
+module.exports = pessoas;
