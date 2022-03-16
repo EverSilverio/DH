4d89e8367bec6c4ee640a138c8be28a5fb2805bd
EverSilverio <ever.silverio@gmail.com>
2022-03-15 21:52:39 -0300

    initial checkpoint_I

---
+++ FrontEnd_II/checkpoint_I/script.js
@@ -0,0 +1,7 @@
+//https://picsum.photos/id/1024/400/300
+//https://picsum.photos/id/237/400/300
+//https://picsum.photos/id/1025/400/300
+//https://picsum.photos/id/1069/400/300
+//https://picsum.photos/id/1074/400/300
+//https://picsum.photos/id/1084/400/300
+//https://picsum.photos/id/169/400/300
