e6fff81466176f3c8f26edfe62cb051c34814c31
EverSilverio <ever.silverio@gmail.com>
2022-03-15 22:53:01 -0300

    Aula 09 - ajustes cod - comentarios

--- FrontEnd_II/aula09/script.js
+++ FrontEnd_II/aula09/script.js
@@ -1,16 +1,13 @@
-//https://picsum.photos/id/1024/400/300
-//https://picsum.photos/id/237/400/300
-//https://picsum.photos/id/1025/400/300
-//https://picsum.photos/id/1069/400/300
-//https://picsum.photos/id/1074/400/300
-//https://picsum.photos/id/1084/400/300
-//https://picsum.photos/id/169/400/300
-
-
+// recebe elemento formulario
 const form = document.querySelector('#form');
+
+// recebe container onde serao adicionados os cards
 const containerCards = document.querySelector('#container-cards');
+
+// lista que ira receber os objetos cards
 var cards = [];
 
+// funcao que cria o template e adiciona o card ao container
 const addCard = function (titulo, descricao, url) {
     const template = `
     <div class="card">
@@ -21,9 +18,9 @@ const addCard = function (titulo, descricao, url) {
     `;
 
     containerCards.innerHTML += template;
-
 }
 
+// event onsubmit - ao clicar botao de add imagens
 form.onsubmit = function (event) {
     var titulo = document.querySelector('#titulo').value;
     var descricao = document.querySelector('#descricao').value;
@@ -39,10 +36,17 @@ form.onsubmit = function (event) {
     // adicionar o card à lista cards
     cards.push(card);
 
+    // executa funcao que add o card ao DOM
     addCard(titulo, descricao, url);
 
-
     event.preventDefault();
-
 }
 
+//exemplos de imagens para testes
+//https://picsum.photos/id/1024/400/300
+//https://picsum.photos/id/237/400/300
+//https://picsum.photos/id/1025/400/300
+//https://picsum.photos/id/1069/400/300
+//https://picsum.photos/id/1074/400/300
+//https://picsum.photos/id/1084/400/300
+//https://picsum.photos/id/169/400/300
