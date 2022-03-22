f60305af7cbe8da17b1f8fa247b5308844fa4785
EverSilverio <ever.silverio@gmail.com>
2022-03-21 21:00:52 -0300

    apoio checkpoint ederson

--- FrontEnd_II/ederson/script.js
+++ FrontEnd_II/ederson/script.js
@@ -1,12 +1,13 @@
 const formulario = document.querySelector(`#formulario`);
 const collectionCards = document.querySelector (`#cards`);
 
-const adicionaCard = function (titulo, descricao, url) {
+const adicionaCard = function (titulo, ano, descricao, url) {
     const template = `
     <div class="card">
-       <img src="${url}" alt="${titulo}" width="400px">
-       <p>${titulo}</p>
-       <p>${descricao}</p>
+       <img src="${url}" alt="${titulo}" width="550px">
+       <h2>${titulo} </h2>
+       <p>Ano: ${ano}</p>
+       <p>Descrição: ${descricao}</p>
      </div>
     `;
 
@@ -16,10 +17,18 @@ const adicionaCard = function (titulo, descricao, url) {
 
 formulario.onsubmit = function (e) {
     var titulo = document.querySelector('#titulo').value;
+    var ano = document.querySelector('#ano').value;
     var descricao = document.querySelector('#descricao').value;
     var url = document.querySelector('#url').value;
-    adicionaCard(titulo,descricao,url);
+    adicionaCard(titulo,ano,descricao,url);
 
     // Previne o comportamento default do evento.
     e.preventDefault();
+}
+
+var removerCards = document.getElementById('remove')
+removerCards.addEventListener('click', removeCards)
+
+function removeCards(){
+    collectionCards.innerHTML='';
 }
