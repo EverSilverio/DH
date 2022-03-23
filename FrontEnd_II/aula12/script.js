677c77257c213c56e4b7f90dcfaf2c201cfdfebe
EverSilverio <ever.silverio@gmail.com>
2022-03-23 08:30:47 -0300

    Melhorias na organizacao do cod e tratativa cor input

--- FrontEnd_II/aula12/script.js
+++ FrontEnd_II/aula12/script.js
@@ -14,8 +14,13 @@ const nacionalidades = document.getElementsByName('nacionalidade')
 // obtem elemento mensagem
 const msg = document.getElementById('msg');
 
+const msg_01 = 'Desculpe, ainda não estamos recrutando bruxos no Brasil!<br> Mas chegaremos em breve!';
+const msg_02 = 'Nome informado inválido';
+const msg_03 = 'Informar no máximo 04 (quatro) hobbies';
+
+
 // funcao para desabilitar o botao
-const funcToggleBtn = function(desab) {
+const funcToggleBtn = function (desab) {
    if (desab) {
       btnSubmit.removeAttribute('disabled');
    } else {
@@ -23,13 +28,8 @@ const funcToggleBtn = function(desab) {
    }
 }
 
-// evento ao sair do campo nome - validacoes nome
-nome.addEventListener('focusout', function(ev) {
-   validacao();
-})
-
 // conta hobbies checados
-const contaCheckes = function() {
+const contaCheckes = function () {
    let cont = 0;
    for (h of hobbies) {
       if (h.checked)
@@ -38,89 +38,110 @@ const contaCheckes = function() {
    return cont;
 }
 
-// evento onchange hobbies
-hobbies.forEach(el => {
-   el.addEventListener('change', function(ev) {
-      if (contaCheckes() > 4) {
-         funcToggleBtn(false);
-         return;
-      }
-      funcToggleBtn(true);
-      validacao();
-   })
-});
+// função de revalidacao geral do formulario 
+const validacao = function () {
+   //valida se possui nome e sobrenome
+   let arrNomecompleto = nome.value.split(' ');
 
-// evento onchange nacionalidade
-nacionalidades.forEach(el => {
-   el.addEventListener('change', function(ev) {
-      console.log(el.checked);
+   // validaca se possui nome e sobrenome
+   if (arrNomecompleto.length > 1) {
+      nome.classList.remove('error');
+      msg.style.display = 'none';
+      msg.innerHTML = '';      
+      funcToggleBtn(true);
+   } else {
+      nome.classList.add('error');
+      msg.style.display = 'block';
+      msg.innerHTML = msg_02;      
+      funcToggleBtn(false);
+      return;
+   }
+
+   // validaca se nome ou sobrenome menor que 2 
+   if (arrNomecompleto[0].length > 1 && arrNomecompleto[1].length > 1) {
+      nome.classList.remove('error');
+      msg.style.display = 'none';
+      msg.innerHTML = '';        
+      funcToggleBtn(true);
+   } else {
+      nome.classList.add('error');
+      msg.style.display = 'block';
+      msg.innerHTML = msg_02;        
+      funcToggleBtn(false);
+      return;
+   }
+
+   // valida se nome excedeu 150 chars
+   if (nome.value.length <= 150 && nome.value !== '') {
+      nome.classList.remove('error');
+      msg.style.display = 'none';
+      msg.innerHTML = '';         
+      funcToggleBtn(true);
+   } else {
+      nome.classList.add('error');
+      msg.style.display = 'block';
+      msg.innerHTML = msg_02;        
+      funcToggleBtn(false)
+      return;
+   }
+
+   // valida se nome possui nro
+   const regexNum = /[0-9]/;
+   if (regexNum.test(nome.value)) {
+      nome.classList.add('error');
+      msg.style.display = 'block';
+      msg.innerHTML = msg_02;          
+      funcToggleBtn(false)
+      return;
+   } else {
+      nome.classList.remove('error');
+      msg.style.display = 'none';
+      msg.innerHTML = '';        
+      funcToggleBtn(true);
+   }
+
+   // valida hobbies
+   if (contaCheckes() > 4) {
+      msg.style.display = 'block';
+      msg.innerHTML = msg_03; 
+      funcToggleBtn(false);
+      return;
+   } else {
+      msg.style.display = 'none';
+      msg.innerHTML = '';            
+      funcToggleBtn(true);
+   }
+
+   // valida nacionalidades
+   nacionalidades.forEach(el => {
+      console.log(el.value);
+
       if (el.checked === true) {
          if (el.value == 'nacionalidadeBrasileira') {
-            funcToggleBtn(false);
             // exibe elemento com mensagem
             msg.style.display = 'block';
+            msg.innerHTML = msg_01;
+            funcToggleBtn(false);
             return;
          }
          // oculta elemento com mensagem
          msg.style.display = 'none';
+         msg.innerHTML = '';
          funcToggleBtn(true);
-         validacao();
       }
-   })
+   });
+}
+
+
+// adiciona evento ao sair do campo nome - validacao
+nome.addEventListener('change', validacao);
+
+// adiciona evento onchange a todos hobbies - validacao
+hobbies.forEach(el => {
+   el.addEventListener('change', validacao)
 });
 
-// revalidacao geral do formulario 
-const validacao = function() {
-   //valida se possui nome e sobrenome
-   let arrNomecompleto = nome.value.split(' ');
-
-   if (arrNomecompleto.length >= 2) {
-      // se nome ou sobrenome menor que 2 desabilita botao
-      if (arrNomecompleto[0].length > 1 && arrNomecompleto[1].length > 1) {
-         funcToggleBtn(true);
-      } else {
-         funcToggleBtn(false);
-         return;
-      }
-
-      // valida se nome excedeu 150 chars
-      if (nome.value.length <= 150 && nome.value !== '') {
-         funcToggleBtn(true);
-      } else {
-         funcToggleBtn(false)
-         return;
-      }
-
-      // valida se nome possui nro
-      const regexNum = /[0-9]/;
-      if (regexNum.test(nome.value)) {
-         funcToggleBtn(false)
-         return;
-      } else {
-         funcToggleBtn(true);
-      }
-
-      // valida hobbies
-      if (contaCheckes() > 4) {
-         funcToggleBtn(false);
-         return;
-      } else {
-         funcToggleBtn(true);
-      }
-
-      // valida nacionalidades
-      nacionalidades.forEach(el => {
-         if (el.checked === true) {
-            if (el.value == 'nacionalidadeBrasileira') {
-               funcToggleBtn(false);
-               // exibe elemento com mensagem
-               msg.style.display = 'block';
-               return;
-            }
-            // oculta elemento com mensagem
-            msg.style.display = 'none';
-            funcToggleBtn(true);
-         }
-      });
-   }
-}
+// adiciona evento onchange nacionalidade - validacao
+nacionalidades.forEach(el => {
+   el.addEventListener('change', validacao)
+});
