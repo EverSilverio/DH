281587de9b5554b3cb6d1cc75fd05ca0b71bc92c
EverSilverio <ever.silverio@gmail.com>
2022-03-30 13:41:53 -0300

    aula 15 - Mesa 11 - validacoes

---
+++ FrontEnd_II/aula15/README.md
@@ -0,0 +1,11 @@
+# EXERCICIO - MESA DE TRABALHO - AULA 15
+
+## Equipe 4
+- Lucas Rosa
+- Guilherme Brasil Tourinho
+- Joaderson Felipe Silva Barbosa
+- Everton Silverio
+- Eduardo Russo
+
+> Utilizar no exercício:
+> validação [x] | normalização [x] | localStorage [x] | location [x] 
--- FrontEnd_II/aula15/scripts/signup.js
+++ FrontEnd_II/aula15/scripts/signup.js
@@ -6,50 +6,62 @@
 // Eduardo Russo
 
 // Utilizar no exercício:
-// validação | normalização | location | localStorage
+// validação [x] | normalização [x] | location | localStorage
+
+const nome = document.getElementById("nome");
+const apelido = document.getElementById("apelido");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+const regexMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 function criarUsuario(ev) {
+   // normalização email
+   email.value = email.value.toLowerCase();
 
-   const nome = document.querySelector("#nome");
-   const apelido = document.querySelector("#apelido");
-   const email = document.querySelector("#email");
-   const password = document.querySelector("#password");
-   const password2 = document.querySelector("#password2");
-
-   if (nome.value.length < 4) {
-      alert("Nome Falhou")
+   // validações do formulario
+   // nome
+   if (nome.value.length < 4 || nome.value.split(' ').length < 2) {
+      nome.focus();
+      alert("Nome Inválido, informar nome e sobrenome!")
       return;
    }
 
-   if (apelido.value.length < 4) {
-      alert("Apelido Falhou")
+   // apelido
+   if (apelido.value.length < 4 || apelido.value.length > 50) {
+      apelido.focus();
+      alert("Apelido deve ter mais de 4 caracteres menos que 50!")
       return;
    }
 
-   if (
-      (!/.com$/.test(email.value) && !/.com.br$/.test(email.value)) ||
-      !email.value.includes('@')
-   ) {
-      alert("Email Falhou")
+   // email
+   if (!regexMail.test(email.value)) {
+      email.focus();
+      alert("Email Inválido")
       return;
    }
 
-   if (password.value.length < 8 && password.value.length > 12) {
-      alert("Senha Falhou")
+   // password
+   if (password.value.length < 8 || password.value.length > 12) {
+      password.focus()
+      alert("Senha deve ter entre 8 e 12 caracteres!")
       return;
    }
 
+   // repetir a senha
    if (password.value != password2.value) {
-      alert("Senha2 Falhou")
+      password2.focus();
+      alert("As senha devem ser iguais!")
       return;
    }
+
+   // armazena dados do login 
+   //--- ATENÇÃO!! SENHA TB ESTÁ SENDO GRAVADA PARA EFEITOS DIDÁTICOS, MAS NÃO É RECOMENDADO
 
    let strg = [nome.value, apelido.value, email.value, password.value];
-
    localStorage.setItem("conta", strg);
-
    alert("Conta Criada com Sucesso");
 
+   // Redireciona pagina
    location.href = 'index.html';
-
 }
