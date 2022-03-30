0258334917996c9b10c3fba8679bc6b00a03ba27
EverSilverio <ever.silverio@gmail.com>
2022-03-29 21:09:17 -0300

    Aula 16 - promisses - ajustes para fazer o laço

--- FrontEnd_II/aula16/promesas-main/js/index.js
+++ FrontEnd_II/aula16/promesas-main/js/index.js
@@ -1,110 +1,111 @@
 const main = document.querySelector('main');
 
 const baseDeDados = {
-    "resultado": [{
-        "genero": "masculino",
-        "nome": {
-            "titulo": "sr",
-            "primeiro": "David",
-            "utlimo": "Fernando"
-        },
-        "localizacao": {
-            "rua": " Augusta , 238",
-            "cidade": "São Paulo",
-            "Estado": "São Paulo",
-            "postcode": "01305000",
-            "coordenadas": {
-                "latitude": "-23.5569581",
-                "longitude": "-46.6589677"
+    "resultado": [
+        {
+            "genero": "masculino",
+            "nome": {
+                "titulo": "sr",
+                "primeiro": "David",
+                "utlimo": "Fernando"
             },
-            "timezone": {
-                "offset": "-3:00",
-                "descricao": "Brasil"
-            }
-        },
-        "email": "david.fernando@exemplo.com",
-        "login": {
-            "uuid": "155e77ee-ba6d-486f-95ce-0e0c0fb4b919",
-            "usuario": "silverswan131",
-            "senha": "firewall",
-            "salt": "TQA1Gz7x",
-            "md5": "dc523cb313b63dfe5be2140b0c05b3bc",
-            "sha1": "7a4aa07d1bedcc6bcf4b7f8856643492c191540d",
-            "sha256": "74364e96174afa7d17ee52dd2c9c7a4651fe1254f471a78bda0190135dcd3480"
-        },
-        "dob": {
-            "data": "1993-07-20T09:44:18.674Z",
-            "idade": 26
-        },
-        "registro": {
-            "data": "2002-05-21T10:59:49.966Z",
-            "idade": 17
-        },
-        "fone": "011-4039-8745",
-        "celular": "011-99874-5621",
-        "id": {
-            "nome": "PPS",
-            "valor": "0390511T"
-        },
-        "imagem": {
-            "grande": "https://randomuser.me/api/portraits/men/75.jpg",
-            "media": "https://randomuser.me/api/portraits/med/men/75.jpg",
-            "miniatura": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-        },
-        "nat": "IE"
-    },
-    {
-        "genero": "masculino",
-        "nome": {
-            "titulo": "sr",
-            "primeiro": "Joaquim",
-            "utlimo": "Ferreira"
-        },
-        "localizacao": {
-            "rua": " Augusta , 238",
-            "cidade": "São Paulo",
-            "Estado": "São Paulo",
-            "postcode": "01305000",
-            "coordenadas": {
-                "latitude": "-23.5569581",
-                "longitude": "-46.6589677"
+            "localizacao": {
+                "rua": " Augusta , 238",
+                "cidade": "São Paulo",
+                "Estado": "São Paulo",
+                "postcode": "01305000",
+                "coordenadas": {
+                    "latitude": "-23.5569581",
+                    "longitude": "-46.6589677"
+                },
+                "timezone": {
+                    "offset": "-3:00",
+                    "descricao": "Brasil"
+                }
             },
-            "timezone": {
-                "offset": "-3:00",
-                "descricao": "Brasil"
-            }
+            "email": "david.fernando@exemplo.com",
+            "login": {
+                "uuid": "155e77ee-ba6d-486f-95ce-0e0c0fb4b919",
+                "usuario": "silverswan131",
+                "senha": "firewall",
+                "salt": "TQA1Gz7x",
+                "md5": "dc523cb313b63dfe5be2140b0c05b3bc",
+                "sha1": "7a4aa07d1bedcc6bcf4b7f8856643492c191540d",
+                "sha256": "74364e96174afa7d17ee52dd2c9c7a4651fe1254f471a78bda0190135dcd3480"
+            },
+            "dob": {
+                "data": "1993-07-20T09:44:18.674Z",
+                "idade": 26
+            },
+            "registro": {
+                "data": "2002-05-21T10:59:49.966Z",
+                "idade": 17
+            },
+            "fone": "011-4039-8745",
+            "celular": "011-99874-5621",
+            "id": {
+                "nome": "PPS",
+                "valor": "0390511T"
+            },
+            "imagem": {
+                "grande": "https://randomuser.me/api/portraits/men/75.jpg",
+                "media": "https://randomuser.me/api/portraits/med/men/75.jpg",
+                "miniatura": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+            },
+            "nat": "IE"
         },
-        "email": "joaquim@exemplo.com",
-        "login": {
-            "uuid": "155e77ee-ba6d-486f-95ce-0e0c0fb4b919",
-            "usuario": "silverswan131",
-            "senha": "firewall",
-            "salt": "TQA1Gz7x",
-            "md5": "dc523cb313b63dfe5be2140b0c05b3bc",
-            "sha1": "7a4aa07d1bedcc6bcf4b7f8856643492c191540d",
-            "sha256": "74364e96174afa7d17ee52dd2c9c7a4651fe1254f471a78bda0190135dcd3480"
-        },
-        "dob": {
-            "data": "1993-07-20T09:44:18.674Z",
-            "idade": 26
-        },
-        "registro": {
-            "data": "2002-05-21T10:59:49.966Z",
-            "idade": 17
-        },
-        "fone": "011-4039-8745",
-        "celular": "011-99874-5621",
-        "id": {
-            "nome": "PPS",
-            "valor": "0390511T"
-        },
-        "imagem": {
-            "grande": "https://randomuser.me/api/portraits/men/75.jpg",
-            "media": "https://randomuser.me/api/portraits/med/men/75.jpg",
-            "miniatura": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-        },
-        "nat": "IE"
-    }],
+        {
+            "genero": "masculino",
+            "nome": {
+                "titulo": "sr",
+                "primeiro": "Joaquim",
+                "utlimo": "Ferreira"
+            },
+            "localizacao": {
+                "rua": " Augusta , 238",
+                "cidade": "São Paulo",
+                "Estado": "São Paulo",
+                "postcode": "01305000",
+                "coordenadas": {
+                    "latitude": "-23.5569581",
+                    "longitude": "-46.6589677"
+                },
+                "timezone": {
+                    "offset": "-3:00",
+                    "descricao": "Brasil"
+                }
+            },
+            "email": "joaquim@exemplo.com",
+            "login": {
+                "uuid": "155e77ee-ba6d-486f-95ce-0e0c0fb4b919",
+                "usuario": "silverswan131",
+                "senha": "firewall",
+                "salt": "TQA1Gz7x",
+                "md5": "dc523cb313b63dfe5be2140b0c05b3bc",
+                "sha1": "7a4aa07d1bedcc6bcf4b7f8856643492c191540d",
+                "sha256": "74364e96174afa7d17ee52dd2c9c7a4651fe1254f471a78bda0190135dcd3480"
+            },
+            "dob": {
+                "data": "1993-07-20T09:44:18.674Z",
+                "idade": 26
+            },
+            "registro": {
+                "data": "2002-05-21T10:59:49.966Z",
+                "idade": 17
+            },
+            "fone": "011-4039-8745",
+            "celular": "011-99874-5621",
+            "id": {
+                "nome": "PPS",
+                "valor": "0390511T"
+            },
+            "imagem": {
+                "grande": "https://randomuser.me/api/portraits/men/76.jpg",
+                "media": "https://randomuser.me/api/portraits/med/men/76.jpg",
+                "miniatura": "https://randomuser.me/api/portraits/thumb/men/76.jpg"
+            },
+            "nat": "IE"
+        }],
     "info": {
         "seed": "fea8be3e64777240",
         "resultado": 1,
@@ -148,24 +149,24 @@ function renderizarDadosUsuario(dados) {
 
     // loop na lista de usuarios
     dados.resultado.forEach(element => {
-        
+
         // obtem resultado do usuario e armazena em usuario
         const usuario = element;
-    
+
         // obtem nome completo
-        const nomeCompleto = 
+        const nomeCompleto =
             // usuario.nome.titulo + ' ' + 
-            usuario.nome.primeiro + ' ' + 
+            usuario.nome.primeiro + ' ' +
             usuario.nome.utlimo;
-    
+
         // obtem email
         const email = usuario.email;
         // obtem foto
         const foto = usuario.imagem.grande;
-    
+
         // cria template card
         const card = templateCard(nomeCompleto, email, foto);
-    
+
         console.log(card);
         // incluir card no HTML
         main.innerHTML += card;
@@ -186,5 +187,5 @@ const templateCard = function (nome, email, img) {
         </div>
     </div>    
     `;
-    
+
 }
